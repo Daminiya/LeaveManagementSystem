eac78be39620e88e5e0a50f51c1d4fce110eda74
Daminiya <daminiyajeya@gmail.com>
2018-09-29 16:38:15 +0530

    Backup of group2

--- src/main/java/com/sgic/hrm/leavesystem/repository/StatusRepository.java
+++
@@ -1,9 +0,0 @@
-package com.sgic.hrm.leavesystem.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.sgic.hrm.leavesystem.entity.Status;
-
-public interface StatusRepository extends JpaRepository<Status,Integer>{
-
-}
