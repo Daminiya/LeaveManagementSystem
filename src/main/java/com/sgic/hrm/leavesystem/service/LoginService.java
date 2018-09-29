59579f27fa3b5d71e0711e59eb18b4fbdde47ae1
unknown <kerthy92@yahoo.com>
2018-09-29 18:01:17 +0530

    Delete conflict files

--- src/main/java/com/sgic/hrm/leavesystem/service/LoginService.java
+++
@@ -1,5 +0,0 @@
-package com.sgic.hrm.leavesystem.service;
-
-public interface LoginService {
-	boolean getLoginVerification(String userName,String password);
-}
--- src/main/java/com/sgic/hrm/leavesystem/service/RoleService.java
+++
@@ -1,9 +0,0 @@
-package com.sgic.hrm.leavesystem.service;
-
-import java.util.Optional;
-
-import com.sgic.hrm.leavesystem.entity.Role;
-
-public interface RoleService {
-	Optional<Role> getRoleById(Integer id);
-}
