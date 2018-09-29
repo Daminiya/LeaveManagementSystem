17c32f79e85eab8feb24b4ae736583c69dc3d92a
ThamilMoorthy <thamilini94moorthy@gmail.com>
2018-09-29 17:30:43 +0530

    Role Service Modified

--- src/main/java/com/sgic/hrm/leavesystem/service/RoleServiceImpl.java
+++ src/main/java/com/sgic/hrm/leavesystem/service/RoleServiceImpl.java
@@ -1,6 +1,5 @@
 package com.sgic.hrm.leavesystem.service;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,8 +10,7 @@ import com.sgic.hrm.leavesystem.repository.RoleRepository;
 
 @Service
 public class RoleServiceImpl implements RoleService {
-	List<Role> role = new ArrayList<>();
-
+	
 	@Autowired
 	private RoleRepository roleRepository;
 
@@ -29,13 +27,13 @@ public class RoleServiceImpl implements RoleService {
 
 	@Override
 	public boolean editRole(Role role, Integer id) {
-		boolean success = false;
+		
 		if (roleRepository.getOne(id) != null) {
 			role.setId(id);
 			roleRepository.save(role);
-			success = true;
+			return true;
 		}
-		return success;
+		return false;
 	}
 
 }
