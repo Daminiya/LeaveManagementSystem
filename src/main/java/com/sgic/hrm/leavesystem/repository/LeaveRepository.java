e3639fedcc64e260156cb48447977feea63fd93d
PawanNayanajith <djnayanajith@gmail.com>
2018-09-30 15:21:36 +0530

    CalculateLieuLeave Modified & Controller Added

--- src/main/java/com/sgic/hrm/leavesystem/repository/LeaveRepository.java
+++ src/main/java/com/sgic/hrm/leavesystem/repository/LeaveRepository.java
@@ -2,8 +2,9 @@
 package com.sgic.hrm.leavesystem.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
-
+import org.springframework.transaction.annotation.Transactional;
 import com.sgic.hrm.leavesystem.entity.Leave;
 
 public interface LeaveRepository extends JpaRepository<Leave, Integer> {
@@ -11,6 +12,8 @@ public interface LeaveRepository extends JpaRepository<Leave, Integer> {
 	@Query("SELECT lv FROM Leave as lv WHERE lv.userId.id=?1 AND lv.leaveTypeId.id=?2 ")
 	Leave getLeaveById(Integer userId, Integer leaveId);
 
+	@Transactional
+	@Modifying
 	@Query("UPDATE Leave as lv SET lv.remainDays = ?1 WHERE lv.id = ?2 ")
 	void setRemainDays(Float update, Integer id);
 
--- src/main/java/com/sgic/hrm/leavesystem/service/CalculateLieuLeaveService.java
+++ src/main/java/com/sgic/hrm/leavesystem/service/CalculateLieuLeaveService.java
@@ -1,5 +1,5 @@
 package com.sgic.hrm.leavesystem.service;
 
 public interface CalculateLieuLeaveService {
-	boolean changeLieuLeaveCount(Integer userID , Integer leaveTypeId, Float update);
+	boolean changeLieuLeaveCount(Integer userId , Integer leaveTypeId, Float update);
 }
