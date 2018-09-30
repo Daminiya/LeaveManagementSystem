afa93993dffc32b46992d843822f0d4ceca8ebc6
PasinduSandakelum <tmpsthilakarathna2017@gmail.com>
2018-09-30 10:55:22 +0530

    LoginController getLoginCredentiails API created

--- src/main/java/com/sgic/hrm/leavesystem/controller/LoginController.java
+++ src/main/java/com/sgic/hrm/leavesystem/controller/LoginController.java
@@ -3,7 +3,10 @@ package com.sgic.hrm.leavesystem.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.sgic.hrm.leavesystem.entity.Login;
@@ -20,5 +23,19 @@ public class LoginController {
 		return loginServices.getAllLoginCredentials();
 	}
 
+	@GetMapping("/login")
+	public ResponseEntity<String> getLoginCredentials(
+			@RequestParam(value = "userName", required = false) String userName,
+			@RequestParam(value = "password", required = false) String password) {
+		
+		if(userName != null && password != null) {
+			String authUser = loginServices.getLoginVerification(userName, password);
+			
+			return new ResponseEntity<>(authUser,HttpStatus.OK);
+			
+		}
+		
+		return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+	}
 	
 }
