0b847205a6336c8288ec68a0d7ad888f5c850626
thirupparan <thirupparan1994@gmail.com>
2018-10-01 13:14:30 +0530

    Add CarryForwardDays Complited in CarryForwardController

---
+++ src/main/java/com/sgic/hrm/leavesystem/controller/CarryForwardController.java
@@ -0,0 +1,34 @@
+package com.sgic.hrm.leavesystem.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.sgic.hrm.leavesystem.entity.CarryForward;
+import com.sgic.hrm.leavesystem.service.CarryForwardService;
+
+@RestController
+public class CarryForwardController {
+	@Autowired
+	CarryForwardService carryForwardService;
+	@GetMapping("carryforward")
+	public List<CarryForward> ViewCarryForward() {
+		return carryForwardService.ViewCarryForward();
+		
+	}
+	@PostMapping("carryforward")
+	public boolean AddCarryForward(@RequestBody CarryForward carryForward ) {
+		if(carryForward.getUserId() != null) {
+			carryForwardService.AddCarryForward(carryForward);
+			return true;
+		}
+		return false;
+		
+	}
+
+	
+}
