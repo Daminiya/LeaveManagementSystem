697c0689f1c1b8a933f5e36eb4092b398dc4a0b2
PasinduSandakelum <tmpsthilakarathna2017@gmail.com>
2018-10-02 09:04:31 +0530

    Push role services and and repo

--- src/main/java/com/sgic/hrm/leavesystem/controller/LoginController.java
+++ src/main/java/com/sgic/hrm/leavesystem/controller/LoginController.java
@@ -8,10 +8,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.sgic.hrm.leavesystem.entity.Login;
+import com.sgic.hrm.leavesystem.model.LoginModel;
 import com.sgic.hrm.leavesystem.service.LoginService;
 
 @RestController
@@ -25,15 +27,17 @@ public class LoginController {
 		return loginService.getAllLoginCredentials();
 	}
 
-	@GetMapping("/login/user")
-	public ResponseEntity<String> getLoginCredentials(HttpSession session,
-			@RequestParam(value = "userName", required = false) String userName,
-			@RequestParam(value = "password", required = false) String password) {
-		if (loginService.getLoginVerification(userName, password)) {
-			session.setAttribute("userName", userName);
-			return new ResponseEntity<>(userName, HttpStatus.OK);
+	@PostMapping("/login")
+	public ResponseEntity<String> getLoginCredentials(HttpSession session, @RequestBody LoginModel loginModel) {
+
+		if (loginService.getLoginVerification(loginModel.getUserName(), loginModel.getPassword())) {
+			session.setAttribute("userName", loginModel.getUserName());
+			return new ResponseEntity<>("Successfully loged in", HttpStatus.OK);
+
 		}
-		return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+
+		return new ResponseEntity<>("User not exists !", HttpStatus.BAD_REQUEST);
+
 	}
 
 }
