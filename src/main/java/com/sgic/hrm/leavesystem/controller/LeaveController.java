6df5db3081085e8ce04ff37f69db71643f44038f
Thiruparan <thirupparan1994@gmail.com>
2018-10-16 13:59:23 +0530

    remaining leave controller done

--- src/main/java/com/sgic/hrm/leavesystem/controller/LeaveController.java
+++ src/main/java/com/sgic/hrm/leavesystem/controller/LeaveController.java
@@ -5,13 +5,14 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.sgic.hrm.leavesystem.entity.Leave;
 import com.sgic.hrm.leavesystem.service.LeaveService;
-
+@CrossOrigin(origins = "http://localhost:4200", maxAge = 3600)
 @RestController
 public class LeaveController {
 	@Autowired
